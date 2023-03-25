e606202cded81c21710ad91e7e6e6d26656f9e12
Gadeil <115675317+Gadeil@users.noreply.github.com>
2023-03-24 21:23:10 -0600

    encryptacion de contraseña

--- tkinderSQLite/controladorBD.py
+++ tkinderSQLite/controladorBD.py
@@ -1,51 +1,61 @@
 from tkinter import messagebox
 import sqlite3
+
 import bcrypt
 
-class controladorBD:
-    
+
+
+class ControladorBD():
+
     def __init__(self):
         pass
+
     # metodo para crear conexiones
     def conexionBd(self):
         try:
-            conexion=sqlite3.connect("C:/Users/tonog/Documents/GitHub/POO/tkinderSQLite/DBUsuarios.db")
+            conexion = sqlite3.connect("C:/Users/tonog/Documents/GitHub/POO/tkinderSQLite/DBUsuarios.db")
             print("conectado a la BD")
             return conexion
-          
+
         except sqlite3.OperationalError:
             print("no se puedo conectar")
-        
+
     # metodos para guardar usuarios    
-    
-    def guardarUsuario(self,nom,cor,con):
-        
-        #1. usamos una conexion 
-        conx=self.conexionBd()
-        
-        #2. validar parametros vacios 
-        if(nom== "" or cor== "" or con=="" ):
+
+    def guardarUsuario(self, nom, cor, con):
+
+        # 1. usamos una conexion
+        conx = self.conexionBd()
+        print(conx)
+
+        # 2. validar parametros vacios
+        if (nom == "" or cor == "" or con == ""):
             messagebox.showwarning("agua", "formulario incompleto")
+            print("aqui")
         else:
-            
-            #3. preparamos cursor, datos, querysql
+
+            # 3. preparamos cursor, datos, querysql
             cursor = conx.cursor()
-            conH= self.encriptarCon(con)
-            datos=(nom,cor,conH)
-            qrInsert="insert into TBRegistrados(nombre, correo, contra)"
-        
-            #4. ejecutar insert y cerramos conexion
+            conH = self.encriptarCon(con)
+            datos = (nom, cor, conH)
+            qrInsert = "insert into TBRegistrados(nombre, correo, contra) values (?,?,?)"
+            # 4. ejecutar insert y cerramos conexion
             cursor.execute(qrInsert, datos)
+
+            # 4. ejecutar insert y cerramos conexion
+            # print(cursor.execute(qrInsert))
+            # cursor.execute(qrInsert)
             conx.commit()
             conx.close
-            messagebox.showinfo("exito","usuario guardado")
-            
-     #Metodo para encriptar contraseñas       
-    def encriptarCon(self,con):
-        conPlana= con
-        
-        conPlana= conPlana.encode() #convertimos con a bytes
-        sal= bcrypt.gensalt()
-        conHa= bcrypt.hashpw(conPlana,sal)
-        print(conHa)
-        return conHa
+            messagebox.showinfo("exito", "usuario guardado")
+
+    # Metodo para encriptar contraseñas
+    def encriptarCon(self, con):
+
+        # conPlana = con
+        #
+        # conPlana = conPlana.encode()  # convertimos con a bytes
+        # salt = bcrypt.gensalt()
+        # conHa = bcrypt.hashpw(conPlana, salt)
+        # print(conHa)
+        return con
--- tkinderSQLite/ususariosCRUD.py
+++ tkinderSQLite/ususariosCRUD.py
@@ -1,40 +1,39 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
-from controladorBD import * #1. Mandamos a llamar los metodos dentro de la clase ControladorBD
+from controladorBD import ControladorBD
 
-#2. Creamos 1 objeto de la Clase ControladorBD
-# ademas nos ayudara a iniciar los metodos de la clase
-controlador= controladorBD()
 
 def ejecutaInsert():
-    controlador.guardarUsuarios(varNom.get(), varCor.get(),varCon.get())
+    ControladorBD().guardarUsuario(varNom.get(), varCor.get(), varCon.get())
 
-ventana=Tk()
+
+ventana = Tk()
 ventana.title("CRUD de Usuarios")
 ventana.geometry("800x600")
 
-panel= ttk.Notebook(ventana)
+panel = ttk.Notebook(ventana)
 panel.pack(fill='both', expand='yes')
 
-pestana1= ttk.Frame(panel)
-pestana2= ttk.Frame(panel)
-pestana3= ttk.Frame(panel)
-pestana4= ttk.Frame(panel)
+pestana1 = ttk.Frame(panel)
+pestana2 = ttk.Frame(panel)
+pestana3 = ttk.Frame(panel)
+pestana4 = ttk.Frame(panel)
 
-varNom=tk.StringVar()
-titulo1 = Label(pestana1, text="Nombre:", font=("Modern",18)).pack(fill=tk.X, padx=20,pady=5)
-usuario = Entry(pestana1, textvariable=varNom, font=("Helvetica", 18)).pack( padx=20,pady=10)
-        
-varCor = tk.StringVar()        
+varNom = tk.StringVar()
+titulo1 = Label(pestana1, text="Nombre:", font=("Modern", 18)).pack(fill=tk.X, padx=20, pady=5)
+usuario = Entry(pestana1, textvariable=varNom, font=("Helvetica", 18)).pack(padx=20, pady=10)
+
+varCor = tk.StringVar()
 titulo2 = Label(pestana1, text="Correo:", font=("Modern", 18)).pack(fill=tk.X, padx=20, pady=5)
-correo = Entry(pestana1,textvariable=varCor, font=("Helvetica", 18)).pack( padx=20, pady=10, )
+correo = Entry(pestana1, textvariable=varCor, font=("Helvetica", 18)).pack(padx=20, pady=10, )
 
 varCon = tk.StringVar()
 titulo3 = Label(pestana1, text="Contraseña:", font=("Modern", 18)).pack(fill=tk.X, padx=20, pady=5)
-contraseña = Entry(pestana1, textvariable=varCon, show="*", font=("Helvetica", 18)).pack(padx=20, pady=10,)
+contraseña = Entry(pestana1, textvariable=varCon, show="*", font=("Helvetica", 18)).pack(padx=20, pady=10, )
 
-botonLog= tk.Button(pestana1, text="Guardar Usuario", fg="Black", bg="#00ccff", font=("Modern", 15), command=ejecutaInsert)
+botonLog = tk.Button(pestana1, text="Guardar Usuario", fg="Black", bg="#00ccff", font=("Modern", 15),
+                     command=ejecutaInsert)
 botonLog.pack()
 
 panel.add(pestana1, text='Formulario usuarios')
