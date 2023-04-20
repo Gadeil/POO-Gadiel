8ed4b66e789e96fc8631d1eb6b4a9b7b454d3cf7
Gadeil <115675317+Gadeil@users.noreply.github.com>
2023-04-19 18:58:33 -0600

    .
    .

--- examen_3/interfaz.py
+++ examen_3/interfaz.py
@@ -11,6 +11,7 @@ panel.pack(fill="both", expand="yes")
 
 pestana1= ttk.Frame(panel)
 pestana2= ttk.Frame(panel)
+pestana3= ttk.Frame(panel)
 
 titulo=Label(pestana1, text="Insertar",fg="black", font=("Modern",18)).pack()
 
@@ -18,13 +19,6 @@ varNom=tk.StringVar()
 etqNom=Label(pestana1,text="Nombre: ").pack()
 txtNom=Entry(pestana1,textvariable=varNom).pack()
 
-varCor=tk.StringVar()
-etqCor=Label(pestana1,text="Correo: ").pack()
-txtCor=Entry(pestana1,textvariable=varCor).pack()
-
-varCon=tk.StringVar()
-etqCon=Label(pestana1,text="Contraseña: ").pack()
-txtCon=Entry(pestana1,textvariable=varCon).pack()
 
 #butGuardar=Button(pestana1,text="Guardar Usuario",command=ejecutaInsert).pack()
 
@@ -39,11 +33,22 @@ txtId=Entry(pestana2,textvariable=varBus).pack()
 
 #btnBus=Button(pestana2,text="Buscar",command=ejecutaSelectU).pack()
 
-subBus=Label(pestana2,text="Encontrado:",fg="blue",font=("Modern",15)).pack()
-txtEnc=tk.Text(pestana2,height=5,width=52).pack()
+
+
+titulo3=Label(pestana3, text="consultar",fg="black", font=("Modern",18)).pack()
+subBus=Label(pestana3,text=":",fg="blue",font=("Modern",15)).pack()
+
+
+varBus=tk.StringVar()
+etqId=Label(pestana3,text="consultar: ").pack()
+
+
+
+
 
 panel.add(pestana1,text="insertar")
 panel.add(pestana2,text="eliminar")
+panel.add(pestana3,text="Consultar ")
 
 
 
